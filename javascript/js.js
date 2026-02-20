e07d138c919d583ce9869c50879bb9e0b01e4715
lantiankuo <3866963934@qq.com>
2026-02-20 14:20:18 +0800

    updata

---
+++ javascript/js.js
@@ -0,0 +1,9 @@
+// new js flie;
+// 2025~2026 The Backrooms LTK(cc by sa);
+// lantiankuo 2020~2026 (copyright);
+// header;
+
+
+function change_color(id, color){
+    document.getElementsByName(id).style.color= "blue" ; 
+}
